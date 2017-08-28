e2d4e7643103e0f90a3be3d022c708a45ed430ae
aijiejie <125361152@qq.com>
2017-08-28 14:31:21 +0800

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,27 @@
+name := "serverActor"
+
+version := "2.2"
+
+scalaVersion := "2.11.3"
+
+//assemblyJarName in assembly := "ServerActor-assembly-2.0.jar"
+
+
+// https://mvnrepository.com/artifact/com.typesafe.akka/akka-actor_2.11
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.5.0"
+
+
+// https://mvnrepository.com/artifact/com.typesafe.akka/akka-remote_2.11
+libraryDependencies += "com.typesafe.akka" % "akka-remote_2.11" % "2.5.0"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.0.0" //% "provided"
+
+libraryDependencies += "org.apache.spark" % "spark-mllib_2.11" % "2.0.0" //% "provided"
+
+
+
+// https://mvnrepository.com/artifact/com.typesafe.akka/akka-protobuf_2.11
+//libraryDependencies += "com.typesafe.akka" % "akka-protobuf_2.11" % "2.5.0"
+
+//libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.0.1"
+
---
+++ src/main/scala/SeverActor.scala
@@ -0,0 +1,126 @@
+import actors._
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+import models.{DecisonTree, RandomForest}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.recommendation._
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class SeverActor() extends Actor {
+  Logger.getLogger("org").setLevel(Level.ERROR)
+
+  override def receive: Receive = {
+    case "connect" => {
+      println("have client")
+      sender() ! "connect ok"
+    }
+
+    case "stop" => context.system.terminate()
+
+    case ClientSubmitTask(dataPath, name) => {
+      sender() ! "收到测试任务"
+      val result = SeverActor.run(dataPath, name)
+      sender() ! TestResult(result)
+    }
+
+    case _: String => {
+      println("无效命令")
+    }
+
+    //Als应答
+    case AlsTask(masterHost, masterPort, datapath, dataResultPath, alsRseultNumber, name, rank, iter, delimiter) => {
+      sender() ! "收到ALS算法任务"
+      /*下面的函数调用以后要改用子actor实现，与主actor隔离，并实行监控，一旦spark任务出错，可以想办法用父actor来结束子actor的spark任务*/
+      val result = SeverActor.Als(datapath, name, dataResultPath, alsRseultNumber, rank, iter, delimiter)
+      if (result) sender() ! "ALS推荐算法任务成功结束"
+    }
+      //决策树应答
+    case DTTask(masterHost, masterPort,dtTrainDataPath, dataPath, modelResultPath, resultPath,numClasses, name, impurity, maxDepth,maxBins ,delimiter) => {
+      sender() ! "收到决策树任务"
+      /*下面的函数调用以后要改用子actor实现，与主actor隔离，并实行监控，一旦spark任务出错，可以想办法用父actor来结束子actor的spark任务*/
+      val result = DecisonTree.decisonTree(dtTrainDataPath, dataPath, name, delimiter, numClasses, modelResultPath, resultPath, impurity, maxDepth, maxBins)
+      sender() ! DTTaskResult(modelResultPath, result.toString, resultPath)
+    }
+
+    case RFTask(masterHost, masterPort, rfTrainData,predictData, modelResult,presictResult,
+    numClasses,numTrees ,name,featureSubsetStrategy, impurity, maxDepth,maxBins ,delimiter) => {
+      sender() ! "收到随机森林任务"//写到这里！！！！
+      val result = RandomForest.randomForest(rfTrainData, predictData, name,featureSubsetStrategy, delimiter, numClasses,numTrees, modelResult, presictResult, impurity, maxDepth, maxBins)
+      sender() ! RFTaskResult(modelResult,result.toString,presictResult)
+    }
+  }
+}
+
+object SeverActor {
+  var serverActor: ActorRef = _
+
+  def main(args: Array[String]): Unit = {
+
+    val host = args(0)
+    val port = args(1)
+    val configStr =
+      s"""
+         |akka.actor.provider = "akka.remote.RemoteActorRefProvider"
+         |akka.remote.netty.tcp.hostname = "$host"
+         |akka.remote.netty.tcp.port = "$port"
+       """.stripMargin
+    val config = ConfigFactory.parseString(configStr)
+    val actorSystem = ActorSystem("MasterActor", config)
+    //val supervisor = actorSystem.actorOf(Props[Supervisor],"Supervisor")//监控server测试
+    //supervisor ! Props(new SeverActor())
+    serverActor = actorSystem.actorOf(Props(new SeverActor()), "Server")
+    Await.ready(actorSystem.whenTerminated, 30.minutes)
+  }
+
+
+/*测试模块*/
+  def run(dataPath: String, name: String): String = {
+    val logFile = dataPath
+    val conf = new SparkConf().setAppName(name).setMaster("spark://master:7077")
+    //val conf = new SparkConf().setAppName(name).setMaster("local")//.setJars(List("C:\\Users\\少辉\\Desktop\\新浪实习\\serverActor\\target\\scala-2.11\\serverActor-assembly-1.0.jar"))
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile)
+    val numAs = logData.filter(line => line.contains("-")).count()
+    val numBs = logData.filter(line => line.contains("+")).count()
+    println("Lines with -: %s, Lines with +: %s".format(numAs, numBs))
+    sc.stop()
+    "Lines with -: %s, Lines with +: %s".format(numAs, numBs)
+  }
+/*ALS模块*/
+  def Als(dataPath: String, name: String, dataResultPath: String, alsResultNumber: Int, rank: Int, numIterations: Int, delimiter: String): Boolean = {
+    //val conf = new SparkConf().setAppName("ALS").setMaster("spark://master:7077")
+    val conf = new SparkConf().setAppName("ALS").setMaster("yarn-client")
+    //val conf = new SparkConf().setAppName("ALS" + name).setMaster("local")
+    val sc = new SparkContext(conf)
+    val data = sc.textFile(dataPath)
+    val ratings = data.map(_.split(delimiter) match { //处理数据
+      case Array(user, item, rate) => //将数据集转化
+        Rating(user.toInt, item.toInt, rate.toDouble)
+    }) //将数据集转化为专用Rating
+    val model = ALS.train(ratings, rank, numIterations, 0.01) //进行模型训练
+    //model.save(sc, "myAls") //保存模型
+    val allRs = model.recommendProductsForUsers(alsResultNumber). //为所有用户推荐n个商品
+      map {
+      case (userID, recommendations) => {
+        var recommendationStr = ""
+        for (r <- recommendations) {
+          recommendationStr += "产品" + r.product + "评分:" + r.rating + ","
+        }
+        if (recommendationStr.endsWith(","))
+          recommendationStr = recommendationStr.substring(0, recommendationStr.length - 1)
+
+        ("用户" + userID, "推荐：" + recommendationStr)
+      }
+    }
+    allRs.coalesce(1).sortByKey().saveAsTextFile(dataResultPath)
+    sc.stop()
+    true
+  }
+
+
+}
